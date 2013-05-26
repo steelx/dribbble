7699db8f2e0894fe79cd2a620d74199b90634990
Steel <ajinkyab1987@gmail.com>
2013-05-26 10:55:41 +0530

    Added node js server for creating quick server on windows.

---
+++ server.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 1337;
+var express = require("express");
+
+var app = express();
+app.use(app.router); //use both root and other routes below
+app.use(express.static(__dirname)); //use static files in ROOT/public folder
+
+app.get("/", function(request, response){ //root dir
+    response.send("Hello!!");
+});
+
+app.listen(port, host);
+console.log("Server running at " + host + ":" + port + " Press CTRL + C to close Server...");
